b3a53251afeb55b635cbe350e8d8fbef8d71c7dd
Marks Meškis <98739089+Ojasik@users.noreply.github.com>
2022-06-13 20:20:36 +0000

    Pievienoti mainīgie, sākuma teksts un pirmais jautājums

--- .ccls-cache/@home@runner@EksamensForCikls/main.cpp
+++ .ccls-cache/@home@runner@EksamensForCikls/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  string atb1, atb2, atb3, atb4, atb5, atb6, atb7, atb8, atb9, atb10;
+  int sum=0;
+  cout<<"Tas ir viktorina par temu - Cikls ar skaitītāju programmēšanas valodā C++";
+  
+  cout<<"\n\n\n1. Kas ir for cikls?\na. Cikls ar pēcnosacījumu\nb. Cikls ar priekšnosacījumu\nc. Cikls ar skaitītāju\nd. Neviens no minētajiem\n\n";
+  cin>>atb1;
+  if(atb1=="c"){
+    sum++;
+  }
+
 }
